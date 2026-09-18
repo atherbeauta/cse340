874bcfa43566eb55176932a66415c01e184b89e1
Kazadi Athanase Muamba <atherbeauta@gmail.com>
2026-09-18 14:32:27 -0600

    Fix W01 site requirements

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ const PORT = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static(path.join(__dirname, 'css')));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/css-assets', express.static(path.join(__dirname, 'css')));
 app.use(bodyParser.urlencoded({ extended: false }));
 const dataDir = path.join(__dirname, 'data');
 if (!fs.existsSync(dataDir)) {
